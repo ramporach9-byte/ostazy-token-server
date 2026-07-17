cdd3f81b8fe6bfb83c329c7655e6334ffc19c771
ramporach9-byte <ramporach9@gmail.com>
2026-07-17 16:53:36 +0300

    Update generateToken.js

--- api/generateToken.js
+++ api/generateToken.js
@@ -1,4 +1,4 @@
-const { RtcTokenBuilder, RtcRole } = require('agora-token');
+const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 
 const APP_ID = 'b72834689283480dacd31102e1e94851';
 const APP_CERTIFICATE = '712faef4700c4bef9dc00ed2c3ee768f';
@@ -15,14 +15,16 @@ module.exports = (req, res) => {
   }
 
   const expireSeconds = 3600;
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const privilegeExpiredTs = currentTimestamp + expireSeconds;
+
   const token = RtcTokenBuilder.buildTokenWithUid(
     APP_ID,
     APP_CERTIFICATE,
     channelName,
     Number(uid),
     RtcRole.PUBLISHER,
-    expireSeconds,
-    expireSeconds
+    privilegeExpiredTs
   );
 
   res.status(200).json({ token: token });
